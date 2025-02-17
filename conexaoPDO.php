b53ed18bca93ca3c3b4cfcf57f437f1007df81ac
unknown <lfdias2203@gmail.com>
2025-02-16 20:42:06 -0400

    Initial commit

---
+++ conexaoPDO.php
@@ -0,0 +1,19 @@
+<?php
+$host = '207.126.166.43'; // Endereço IP do servidor do banco de dados
+$dbname = 'ecommerce';    // Nome do banco de dados
+$username = 'root';       // Usuário do banco de dados
+$password = '';  // Senha do banco de dados (substitua pela senha correta)
+
+try {
+    // Conexão com o banco de dados usando PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    
+    // Configura o PDO para lançar exceções em caso de erros
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Conexão com o banco de dados estabelecida com sucesso!";
+} catch (PDOException $e) {
+    // Em caso de erro, exibe a mensagem de erro
+    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+}
+?>
