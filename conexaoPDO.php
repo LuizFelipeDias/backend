c344cedfaa096eb33ae3e6a91729fcebfa7f508e
Luiz Felipe Dias de Araujo <141677885+LuizFelipeDias@users.noreply.github.com>
2025-02-17 00:10:13 -0400

    Update conexaoPDO.php

--- conexaoPDO.php
+++ conexaoPDO.php
@@ -1,7 +1,7 @@
 <?php
 $host = '207.126.166.43'; // Endereço IP do servidor do banco de dados
 $dbname = 'ecommerce';    // Nome do banco de dados
-$username = 'root';       // Usuário do banco de dados
+$username = 'railway_user';       // Usuário do banco de dados
 $password = 'L&fda220320002';  // Senha do banco de dados (substitua pela senha correta)
 
 try {
