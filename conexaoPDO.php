680aece809dd90d6fa1c71691b840bd5fd6adc12
Luiz Felipe Dias de Araujo <141677885+LuizFelipeDias@users.noreply.github.com>
2025-02-17 13:51:58 -0400

    Update conexaoPDO.php

--- conexaoPDO.php
+++ conexaoPDO.php
@@ -12,16 +12,4 @@ echo json_encode([
     'password' => $password
 ]);
 
-try {
-    // Conexão com o banco de dados usando PDO
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    
-    // Configura o PDO para lançar exceções em caso de erros
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    echo "Conexão com o banco de dados estabelecida com sucesso!";
-} catch (PDOException $e) {
-    // Em caso de erro, exibe a mensagem de erro
-    die("Erro na conexão com o banco de dados: " . $e->getMessage());
-}
 ?>
