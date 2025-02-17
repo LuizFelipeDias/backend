eba687f5c0a2d3e8b23277b8a346caa51dc50106
Luiz Felipe Dias de Araujo <141677885+LuizFelipeDias@users.noreply.github.com>
2025-02-17 13:44:20 -0400

    Update conexaoPDO.php

--- conexaoPDO.php
+++ conexaoPDO.php
@@ -4,6 +4,14 @@ $dbname = 'ecommerce';    // Nome do banco de dados
 $username = 'root';       // Usuário do banco de dados
 $password = 'L&fda220320002';  // Senha do banco de dados (substitua pela senha correta)
 
+// Retorna as credenciais em formato JSON
+echo json_encode([
+    'host' => $host,
+    'dbname' => $dbname,
+    'username' => $username,
+    'password' => $password
+]);
+
 try {
     // Conexão com o banco de dados usando PDO
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
