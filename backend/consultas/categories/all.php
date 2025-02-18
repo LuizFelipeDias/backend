59de2e17ca91b0889ed99dfdf0ba3db961d834b2
Luiz Felipe Dias de Araujo <141677885+LuizFelipeDias@users.noreply.github.com>
2025-02-17 20:14:14 -0400

    Update all.php

--- backend/consultas/categories/all.php
+++ backend/consultas/categories/all.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 function loadPDOConnection() {
     // URL da API que retorna as credenciais
     $apiUrl = 'https://backend-production-6806.up.railway.app/conexaoPDO.php';
